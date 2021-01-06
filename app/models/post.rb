ec254c7da09439765bc084d2d9369bde3823dc53
orange-lemon888 <techaya875@gmail.com>
2021-01-06 10:34:24 +0000

    add varidation, HTML Syntax error fix

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,5 +6,8 @@ class Post < ApplicationRecord
   has_many :favorites,dependent: :destroy     #行きたいねをしているユーザへの参照
   has_many :users, through: :favorites        #行きたいねをしているユーザ(複数）favoritesのuser_id参照
   
+  validates :title, presence: true, length: { maximum: 50 }
+  validates :area, length: { maximum: 50 }
+  validates :subtitle, length: { maximum: 100 }
   validates :content, presence: true, length: { maximum: 255 }
 end
