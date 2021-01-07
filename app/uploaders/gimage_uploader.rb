22e046a165c9b752e658f43710e387af4a922f2d
orange-lemon888 <techaya875@gmail.com>
2021-01-07 11:34:09 +0000

    for heroku test

--- app/uploaders/gimage_uploader.rb
+++ app/uploaders/gimage_uploader.rb
@@ -39,11 +39,6 @@ class GimageUploader < CarrierWave::Uploader::Base
   #  process :resize_to_limit => [300, 300]
   #end
 
-# jpg,jpeg,gif,pngのみ
-  #def extension_white_list
-  #  %w(jpg jpeg gif png)
-  #end
-
 #ファイル名を変更し拡張子を同じにする
   def filename
     super.chomp(File.extname(super)) + '.jpg' 
@@ -72,9 +67,9 @@ class GimageUploader < CarrierWave::Uploader::Base
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
-  #def extension_whitelist
-  # %w(jpg jpeg gif png)
-  #end
+  def extension_whitelist
+   %w(jpg jpeg gif png)
+  end
 
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
